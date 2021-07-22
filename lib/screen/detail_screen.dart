1674dc1971951a5161654fef0cb0a2b0b75af794
solehfuddin <solehfuddin@gmail.com>
2021-07-22 08:20:38 +0700

    Perbaikan ketika landscape orientation

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -24,54 +24,56 @@ class DetailScreen extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppbarWidget(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                title: '',
-              ),
-              DetailHeaderWidget(
-                logo: actComp.logo,
-                bagian: actComp.bagian,
-                durasi: actComp.durasi,
-                lokasi: actComp.lokasi,
-              ),
-              Center(
-                child: FavoriteButtonWidget(),
-              ),
-              DetailContentWidget(
-                title: 'Persyaratan : ',
-                list: actComp.persyaratan,
-              ),
-              DetailContentWidget(
-                title: 'Benefit : ',
-                list: actComp.benefit,
-              ),
-              DynamicButtonWidget(
-                outerPadding: 15,
-                innerPadding: 8,
-                fontSize: 14,
-                borderRadius: 20,
-                title: 'Ajukan Magang',
-                fontFamily: 'Nunito',
-                fontWeight: FontWeight.w600,
-                fontColor: Colors.white,
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: Text("Berhasil mengajukan magang"),
-                        );
-                      });
-                },
-                borderColor: Color(0xff35466A),
-                bgColor: Color(0xff35466A),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppbarWidget(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  title: '',
+                ),
+                DetailHeaderWidget(
+                  logo: actComp.logo,
+                  bagian: actComp.bagian,
+                  durasi: actComp.durasi,
+                  lokasi: actComp.lokasi,
+                ),
+                Center(
+                  child: FavoriteButtonWidget(),
+                ),
+                DetailContentWidget(
+                  title: 'Persyaratan : ',
+                  list: actComp.persyaratan,
+                ),
+                DetailContentWidget(
+                  title: 'Benefit : ',
+                  list: actComp.benefit,
+                ),
+                DynamicButtonWidget(
+                  outerPadding: 15,
+                  innerPadding: 8,
+                  fontSize: 14,
+                  borderRadius: 20,
+                  title: 'Ajukan Magang',
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.w600,
+                  fontColor: Colors.white,
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("Berhasil mengajukan magang"),
+                          );
+                        });
+                  },
+                  borderColor: Color(0xff35466A),
+                  bgColor: Color(0xff35466A),
+                ),
+              ],
+            ),
           ),
         ),
       ),
